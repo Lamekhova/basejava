3b02e828ab7aba3935c9a343f04a04396be1376c
Ламехова Алиса Александровна <alamehova@air.planeta-avto.ru>
2018-07-05 17:11:11 +0500

    домашнее задание на проверку

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,80 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
+    // Обнуляем массив
     void clear() {
+        Arrays.fill(storage, null);
     }
 
+    // Находим первую ячейку == null, в нее добавляем резюме, выходим из цикла
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
+    // Возвращаем резюме если найдено, иначе возвращаем null
     Resume get(String uuid) {
-        return null;
+        Resume findResume = null;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                findResume = storage[i];
+                break;
+            }
+        }
+        return findResume;
     }
 
+    // Удаляем резюме из storage
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].uuid == uuid) {
+                for (int j = i; j < storage.length - 1; j++) {
+                    if (storage[j + 1] != null) {
+                        storage[j] = storage[j+1];
+                    } else {
+                        storage[j] = null;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
+    // Не нашла я метод в Arrays
     Resume[] getAll() {
-        return new Resume[0];
+        ArrayList<Resume> tempList = new ArrayList();
+        for (Resume element : storage) {
+            if (element != null) {
+                tempList.add(element);
+            } else {
+                break;
+            }
+        }
+        return tempList.toArray(new Resume[0]);
     }
 
     int size() {
-        return 0;
+        int size = 0;
+        for (Resume element : storage) {
+            if (element != null) {
+                size++;
+            } else
+                break;
+        }
+        return size;
     }
 }
--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -11,15 +11,17 @@ public class MainTestArrayStorage {
         r2.uuid = "uuid2";
         Resume r3 = new Resume();
         r3.uuid = "uuid3";
+        Resume r5 = new Resume();
+        r5.uuid = "uuid5";
 
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
+        ARRAY_STORAGE.save(r5);
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.uuid));
         System.out.println("Size: " + ARRAY_STORAGE.size());
-
-        System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
+        System.out.println("Get uuid3: " + ARRAY_STORAGE.get("uuid3"));
 
         printAll();
         ARRAY_STORAGE.delete(r1.uuid);
