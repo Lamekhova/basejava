534eb31fec039ef8ffb4d732e223be073c0b6f10
Alisa <bortkevichtsk@gmail.com>
2018-07-07 18:10:20 +0500

    Исправленное 3

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,43 +4,43 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    Resume[] storage = new Resume[1000];
     int size = 0;
 
     // Обнуляем массив
     void clear() {
-        if (size > 0) {
             Arrays.fill(storage, 0, size, null);
-        }
         size = 0;
     }
 
     // Находим первую ячейку == null, в нее добавляем резюме, выходим из цикла
     void save(Resume r) {
-        storage[size] = r;
-        size++;
+        if (size < storage.length) {
+            storage[size] = r;
+            size++;
+        }
+
     }
 
     // Возвращаем резюме если найдено, иначе возвращаем null
     Resume get(String uuid) {
-        Resume findResume = null;
-        for (int i = 0; i < storage.length; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                findResume = storage[i];
-                break;
+                return storage[i];
             }
         }
-        return findResume;
+        return null;
     }
 
     // Удаляем резюме из storage
     void delete(String uuid) {
-        for (int i = 0; i < size-1; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 System.arraycopy(storage, i + 1, storage, i, storage.length - i - 1);
+                size--;
+                break;
             }
         }
-        size--;
     }
 
     /**
