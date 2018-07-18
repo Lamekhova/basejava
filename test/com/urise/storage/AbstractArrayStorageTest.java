bf9286e12ed8a6d121e50cb45122dee336412f0e
Lamehova <bortkevichtsk@gmail.com>
2018-07-18 12:34:47 +0500

    Added unit tests

---
+++ test/com/urise/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,64 @@
+package com.urise.storage;
+
+import com.urise.exception.ExistStorageExeption;
+import com.urise.exception.NotExistStorageExeption;
+import com.urise.model.Resume;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public abstract class AbstractArrayStorageTest {
+    private Storage storage;
+
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+
+    public AbstractArrayStorageTest(Storage storage) {
+        this.storage = storage;
+    }
+
+    @Before
+    public void setUp() {
+        storage.clear();
+        storage.save(new Resume(UUID_1));
+        storage.save(new Resume(UUID_2));
+        storage.save(new Resume(UUID_3));
+    }
+
+    @Test
+    public void size() {
+        Assert.assertEquals(3, storage.size());
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test(expected = NotExistStorageExeption.class)
+    public void getNotExist() {
+        storage.get("uuid4");
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+}
---
+++ test/com/urise/storage/SortedArrayStorageTest.java
@@ -0,0 +1,22 @@
+package com.urise.storage;
+
+import org.junit.Test;
+
+public class SortedArrayStorageTest extends AbstractArrayStorageTest {
+
+    public SortedArrayStorageTest(Storage storage) {
+        super(storage);
+    }
+
+    @Test
+    public void getIndex() {
+    }
+
+    @Test
+    public void insertElement() {
+    }
+
+    @Test
+    public void shiftElement() {
+    }
+}
