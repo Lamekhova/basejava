b8ee921d9f980e09667605d7ea1fe960841c022b
Alisa <bortkevichtsk@gmail.com>
2018-08-14 23:08:05 +0500

    SuiteTestClass added

---
+++ test/com/urise/storage/SuiteTestClass.java
@@ -0,0 +1,11 @@
+package com.urise.storage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import static org.junit.runners.Suite.*;
+
+@RunWith(Suite.class)
+@SuiteClasses({ArrayStorageTest.class, ListStorageTest.class, MapStorageTest.class, SortedArrayStorageTest.class} )
+public class SuiteTestClass {
+
+}
