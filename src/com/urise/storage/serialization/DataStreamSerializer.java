43ff932ec4abcf3bdd420628e969a84d5d7a3045
Alisa <bortkevichtsk@gmail.com>
2018-09-09 21:05:23 +0500

    DataStreamSerializer HW10

--- src/com/urise/storage/serialization/DataStreamSerializer.java
+++ src/com/urise/storage/serialization/DataStreamSerializer.java
@@ -6,15 +6,22 @@ import java.io.*;
 import java.time.YearMonth;
 import java.util.*;
 import java.util.Map;
+import java.util.function.Predicate;
 
 /**
  * save and restore file in DataStreamFormat
  */
 public class DataStreamSerializer implements Serializer {
 
+    private Predicate<String> isPersonalSectionType = s -> SectionType.valueOf(s).equals(SectionType.PERSONAL);
+    private Predicate<String> isObjectiveSectionType = s -> SectionType.valueOf(s).equals(SectionType.OBJECTIVE);
+    private Predicate<String> isAchievementSectionType = s -> SectionType.valueOf(s).equals(SectionType.ACHIEVEMENT);
+    private Predicate<String> isQualificationSectionType = s -> SectionType.valueOf(s).equals(SectionType.QUALIFICATION);
+    private Predicate<String> isExperienceSectionType = s -> SectionType.valueOf(s).equals(SectionType.EXPERIENCE);
+    private Predicate<String> isEducationSectionType = s -> SectionType.valueOf(s).equals(SectionType.EDUCATION);
+
     @Override
     public void doWrite(Resume resume, OutputStream outputStream) throws IOException {
-
         try (DataOutputStream dataOutputStream = new DataOutputStream(outputStream)) {
             dataOutputStream.writeUTF(resume.getUuid());
             dataOutputStream.writeUTF(resume.getFullName());
@@ -24,18 +31,19 @@ public class DataStreamSerializer implements Serializer {
                 dataOutputStream.writeUTF(entry.getValue());
             }
             for (Map.Entry<SectionType, Section> entry : resume.getResumeSection().entrySet()) {
-                if (entry.getKey().equals(SectionType.PERSONAL) || entry.getKey().equals(SectionType.OBJECTIVE)) {
-                    dataOutputStream.writeUTF(entry.getKey().name());
+                String entryName = entry.getKey().name();
+                if (isPersonalSectionType.test(entryName) || isObjectiveSectionType.test(entryName)) {
+                    dataOutputStream.writeUTF(entryName);
                     dataOutputStream.writeUTF(entry.getValue().toString());
-                } else if (entry.getKey().equals(SectionType.ACHIEVEMENT) || entry.getKey().equals(SectionType.QUALIFICATION)) {
-                    dataOutputStream.writeUTF(entry.getKey().name());
+                } else if (isAchievementSectionType.test(entryName) || isQualificationSectionType.test(entryName)) {
+                    dataOutputStream.writeUTF(entryName);
                     ListSection listSection = (ListSection) entry.getValue();
                     dataOutputStream.writeInt(listSection.getSections().size());
                     for (String section : listSection) {
                         dataOutputStream.writeUTF(section);
                     }
-                } else if ((entry.getKey().equals(SectionType.EXPERIENCE) || entry.getKey().equals(SectionType.EDUCATION))) {
-                    dataOutputStream.writeUTF(entry.getKey().name());
+                } else if ((isExperienceSectionType.test(entryName) || isEducationSectionType.test(entryName))) {
+                    dataOutputStream.writeUTF(entryName);
                     ExperienceSection experienceSection = (ExperienceSection) entry.getValue();
                     dataOutputStream.writeInt(experienceSection.getExperienceEntries().size());
                     for (ExperienceEntry experienceEntry : experienceSection.getExperienceEntries()) {
@@ -66,7 +74,6 @@ public class DataStreamSerializer implements Serializer {
             String fullName = dataInputStream.readUTF();
             Resume resume = new Resume(uuid, fullName);
             int size = dataInputStream.readInt();
-
             for (int i = 0; i < size; i++) {
                 ContactType contactType = ContactType.valueOf(dataInputStream.readUTF());
                 String value = dataInputStream.readUTF();
@@ -74,75 +81,55 @@ public class DataStreamSerializer implements Serializer {
             }
             while (true) {
                 String sectionTypeFromFile = dataInputStream.readUTF();
-                System.out.println(sectionTypeFromFile);
-                if (SectionType.valueOf(sectionTypeFromFile).equals(SectionType.PERSONAL)) {
+                if (isPersonalSectionType.test(sectionTypeFromFile)) {
                     resume.addSection(SectionType.PERSONAL, new TextSection(dataInputStream.readUTF()));
-                } else if (SectionType.valueOf(sectionTypeFromFile).equals(SectionType.OBJECTIVE)) {
+                } else if (isObjectiveSectionType.test(sectionTypeFromFile)) {
                     resume.addSection(SectionType.OBJECTIVE, new TextSection(dataInputStream.readUTF()));
-                } else if (SectionType.valueOf(sectionTypeFromFile).equals(SectionType.ACHIEVEMENT)) {
-                    int numberOfSections = dataInputStream.readInt();
-                    ListSection listSection = new ListSection();
-                    for (int sectionNumber = 0; sectionNumber < numberOfSections; sectionNumber++) {
-                        listSection.getSections().add(dataInputStream.readUTF());
-                    }
-                    resume.addSection(SectionType.ACHIEVEMENT, listSection);
-
-                } else if (SectionType.valueOf(sectionTypeFromFile).equals(SectionType.QUALIFICATION)) {
-                    int numberOfSections = dataInputStream.readInt();
-                    ListSection listSection = new ListSection();
-                    for (int sectionNumber = 0; sectionNumber < numberOfSections; sectionNumber++) {
-                        listSection.getSections().add(dataInputStream.readUTF());
-                    }
-                    resume.addSection(SectionType.QUALIFICATION, listSection);
-
-                } else if (SectionType.valueOf(sectionTypeFromFile).equals(SectionType.EXPERIENCE)) {
-                    int numberOfExperienceEntries = dataInputStream.readInt();
-                    List<ExperienceEntry> experienceEntryList = new ArrayList<>();
-                    for (int experienceEntryNumber = 0; experienceEntryNumber < numberOfExperienceEntries; experienceEntryNumber++) {
-                        String name = dataInputStream.readUTF();
-                        String url = dataInputStream.readUTF();
-                        url = url.equals("null") ?  null : url;
-                        List<Position> listOfPositions = new ArrayList<>();
-                        ExperienceEntry experienceEntry = new ExperienceEntry(name, url, listOfPositions);
-                        int numberOfPositions = dataInputStream.readInt();
-                        for (int positionNumber = 0; positionNumber < numberOfPositions; positionNumber++) {
-                            YearMonth startDate = YearMonth.parse(dataInputStream.readUTF());
-                            YearMonth endDate = YearMonth.parse(dataInputStream.readUTF());
-                            String title = dataInputStream.readUTF();
-                            String description = dataInputStream.readUTF();
-                            description = description.equals("null") ?  null : description;
-                            Position position = new Position(startDate, endDate, title, description);
-                            listOfPositions.add(position);
-                        }
-                        experienceEntryList.add(experienceEntry);
-                    }
-                    resume.addSection(SectionType.EXPERIENCE, new ExperienceSection(experienceEntryList));
-                } else if ((SectionType.valueOf(sectionTypeFromFile).equals(SectionType.EDUCATION))) {
-                    int numberOfExperienceEntries = dataInputStream.readInt();
-                    List<ExperienceEntry> experienceEntryList = new ArrayList<>();
-                    for (int experienceEntryNumber = 0; experienceEntryNumber < numberOfExperienceEntries; experienceEntryNumber++) {
-                        String name = dataInputStream.readUTF();
-                        String url = dataInputStream.readUTF();
-                        url = url.equals("null") ?  null : url;
-                        List<Position> listOfPositions = new ArrayList<>();
-                        ExperienceEntry experienceEntry = new ExperienceEntry(name, url, listOfPositions);
-                        int numberOfPositions = dataInputStream.readInt();
-                        for (int positionNumber = 0; positionNumber < numberOfPositions; positionNumber++) {
-                            YearMonth startDate = YearMonth.parse(dataInputStream.readUTF());
-                            YearMonth endDate = YearMonth.parse(dataInputStream.readUTF());
-                            String title = dataInputStream.readUTF();
-                            String description = dataInputStream.readUTF();
-                            description = description.equals("null") ?  null : description;
-                            Position position = new Position(startDate, endDate, title, description);
-                            listOfPositions.add(position);
-                        }
-                        experienceEntryList.add(experienceEntry);
-                }
-                    resume.addSection(SectionType.EDUCATION, new ExperienceSection(experienceEntryList));
+                } else if (isAchievementSectionType.test(sectionTypeFromFile)) {
+                    resume.addSection(SectionType.ACHIEVEMENT, readListSection(dataInputStream));
+                } else if (isQualificationSectionType.test(sectionTypeFromFile)) {
+                    resume.addSection(SectionType.QUALIFICATION, readListSection(dataInputStream));
+                } else if (isExperienceSectionType.test(sectionTypeFromFile)) {
+                    resume.addSection(SectionType.EXPERIENCE, new ExperienceSection(readMapSection(dataInputStream)));
+                } else if (isEducationSectionType.test(sectionTypeFromFile)) {
+                    resume.addSection(SectionType.EDUCATION, new ExperienceSection(readMapSection(dataInputStream)));
                     break;
                 }
             }
-                return resume;
+            return resume;
         }
+    }
+
+    public ListSection readListSection(DataInputStream dataInputStream) throws IOException {
+        int numberOfSections = dataInputStream.readInt();
+        ListSection listSection = new ListSection();
+        for (int sectionNumber = 0; sectionNumber < numberOfSections; sectionNumber++) {
+            listSection.getSections().add(dataInputStream.readUTF());
+        }
+        return listSection;
+    }
+
+    public List<ExperienceEntry> readMapSection(DataInputStream dataInputStream) throws IOException {
+        int numberOfExperienceEntries = dataInputStream.readInt();
+        List<ExperienceEntry> experienceEntryList = new ArrayList<>();
+        for (int experienceEntryNumber = 0; experienceEntryNumber < numberOfExperienceEntries; experienceEntryNumber++) {
+            String name = dataInputStream.readUTF();
+            String url = dataInputStream.readUTF();
+            url = url.equals("null") ? null : url;
+            List<Position> listOfPositions = new ArrayList<>();
+            ExperienceEntry experienceEntry = new ExperienceEntry(name, url, listOfPositions);
+            int numberOfPositions = dataInputStream.readInt();
+            for (int positionNumber = 0; positionNumber < numberOfPositions; positionNumber++) {
+                YearMonth startDate = YearMonth.parse(dataInputStream.readUTF());
+                YearMonth endDate = YearMonth.parse(dataInputStream.readUTF());
+                String title = dataInputStream.readUTF();
+                String description = dataInputStream.readUTF();
+                description = description.equals("null") ? null : description;
+                Position position = new Position(startDate, endDate, title, description);
+                listOfPositions.add(position);
+            }
+            experienceEntryList.add(experienceEntry);
+        }
+        return experienceEntryList;
     }
 }
