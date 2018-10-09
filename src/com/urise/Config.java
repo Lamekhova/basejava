970f0dee772d409ac722e66f7e57f2416dd8cb0c
Alisa <bortkevichtsk@gmail.com>
2018-10-09 21:01:32 +0500

    Servlet added

--- src/com/urise/Config.java
+++ src/com/urise/Config.java
@@ -6,8 +6,8 @@ import java.io.*;
 import java.util.Properties;
 
 public class Config {
-    private final static Config INSTANCE = new Config();
-    private final static String PATH_TO_PROPERTIES = "C:\\basejava\\config\\resumes_properties";
+    private final File PROPS = new File(getHomeDir(),"\\config\\resumes_properties");
+    private static final Config INSTANCE = new Config();
 
     private final File storageDir;
     private final SqlStorage storage;
@@ -17,8 +17,7 @@ public class Config {
     }
 
     private Config() {
-        File propertiesFile = new File(PATH_TO_PROPERTIES);
-        try (InputStream inputStream = new FileInputStream(propertiesFile)) {
+        try (InputStream inputStream = new FileInputStream(PROPS)) {
             Properties properties = new Properties();
             properties.load(inputStream);
             storageDir = new File(properties.getProperty("storage.dir"));
@@ -28,7 +27,7 @@ public class Config {
                     properties.getProperty("db.password")
             );
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file" + propertiesFile.getAbsolutePath());
+            throw new IllegalStateException("Invalid config file" + PROPS.getAbsolutePath());
         }
     }
 
@@ -38,5 +37,14 @@ public class Config {
 
     public SqlStorage getStorage() {
         return storage;
+    }
+
+    private static File getHomeDir() {
+        String prop = System.getProperty("homeDir");
+        File homeDir = new File(prop == null ? "." : prop);
+        if (!homeDir.isDirectory()) {
+            throw new IllegalStateException(homeDir + " is not directory");
+        }
+        return homeDir;
     }
 }
